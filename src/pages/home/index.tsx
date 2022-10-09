b2904b4aa26e1221b82f72c614b0e7bfaa3c1380
Mardeson Pereira <maxpb777@gmail.com>
2022-10-09 14:49:01 -0300

    feat: finish home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,41 @@
+import { Banner } from './components/Banner';
+import { Header } from '~/components/Header';
 import { Container } from './styles';
+import { Skills } from './components/Skills';
+import { GetServerSideProps } from 'next';
+import { api } from '~/services/api';
+import { SkillDTO } from '~/dtos/SkillDTO';
+import { Contacts } from './components/Contacts';
 
-export default function Home() {
+interface Props {
+  skills: SkillDTO[];
+}
+
+export default function Home({ skills }: Props) {
   return (
     <Container>
-      <h1>Home</h1>
+      <Header />
+
+      <Banner />
+
+      <Skills skills={skills} />
+
+      <Contacts />
+
+      <footer>
+        <span>
+          Desenvolvido com <span>❤</span> por <strong>Mardeson Pereira</strong>
+        </span>
+      </footer>
     </Container>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data } = await api.get('/skill');
+  return {
+    props: {
+      skills: data,
+    },
+  };
+};
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,3 +1,18 @@
 import styled from 'styled-components';
 
-export const Container = styled.section``;
+export const Container = styled.main`
+  footer {
+    padding: 64px 16px;
+    display: flex;
+    justify-content: center;
+
+    span {
+      font-size: 1rem;
+
+      span {
+        color: #ce1900;
+        font-size: 1.2rem;
+      }
+    }
+  }
+`;
